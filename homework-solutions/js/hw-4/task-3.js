e110e50fcd872cd75da0ab3cbda6460fc0ab4749
EphimSh <ephimizm@gmail.com>
2025-08-21 09:42:32 +0300

    add hw4 task-3 solution

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,22 @@
   преобразовываясь в number
 
 */
+function ageCheck(item) {
+  const minAge = 18;
+  const maxAge = 60;
+  age = Number(item);
+  if (age < minAge) {
+    console.log(`You don't have access cause your age is ${age} It's less then ${minAge}`);
+  } else if (age >= minAge && age < maxAge) {
+    console.log(`Welcome  !`);
+  } else if (age >= maxAge) {
+    console.log(`Keep calm and look Culture channel`);
+  } else {
+    console.log(`Technical work`);
+  }
+}
+
+const testData = [10, 17, 18, 19, 59, 60, 61, 101, '100', '2', '-5', 'five'];
+testData.forEach((item) => {
+  ageCheck(item);
+});
