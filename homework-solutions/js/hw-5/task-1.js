c7c96281d55a2522f2b0cb422756e48bc352677a
EphimSh <ephimizm@gmail.com>
2025-08-22 21:23:06 +0300

    add hw5 task-1 solution

--- homework-solutions/js/hw-5/task-1.js
+++ homework-solutions/js/hw-5/task-1.js
@@ -6,6 +6,13 @@
 
 let evenNumbersResult = '';
 
+for(let i = 10; i >= 0; i-= 2){
+    evenNumbersResult += i;
+    if(i > 0) evenNumbersResult += '-';
+}
+
+// console.log(evenNumbersResult);
+
 /**
  * Создать строку из 5 строк с увеличивающимся количеством смайликов ":)".
  * Переменная для результата `smilePatternResult` уже создана и содержит пустую строку.
@@ -18,13 +25,28 @@ let evenNumbersResult = '';
  */
 
 let smilePatternResult = '';
+for(let i = 0; i < 5; i++){
+    for(j = 0; j <= i; j++){
+        smilePatternResult += ':)'    
+    }
+    smilePatternResult += '\n';
+}
+
+// console.log(smilePatternResult);
 
 /**
  * Заменить все пробелы в переменной text на "1".
  * Переменная для результата `replaceSpacesWithOneResult` уже создана и содержит пустую строку.
  * Ожидаемый результат: 'Hello!1I1am1a1JS1student!'
  */
+
 const text = 'Hello! I am a JS student!';
 let replaceSpacesWithOneResult = '';
+for(let i = 0; i < text.length; i++){
+    replaceSpacesWithOneResult += text[i] === ' ' ? "1" : text[i]; 
+};
+console.log(replaceSpacesWithOneResult);
+// replaceSpacesWithOneResult = text.replaceAll(' ', "1"); 
+// console.log(replaceSpacesWithOneResult);
 
-export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
+// export { evenNumbersResult, smilePatternResult, replaceSpacesWithOneResult };
