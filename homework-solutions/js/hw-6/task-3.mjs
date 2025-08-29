277dd19cc4a6c06da8c02f251638df10b6a7dc07
EphimSh <ephimizm@gmail.com>
2025-08-29 21:31:59 +0300

    add hw6 task-3

--- homework-solutions/js/hw-6/task-3.mjs
+++ homework-solutions/js/hw-6/task-3.mjs
@@ -11,7 +11,6 @@ const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 4, 6, 8, 10, 1, 3, 5, 7, 9];
 let duplicateFound;
 do {
   duplicateFound = false;
-
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = nums.length - 1; j > i; j--) {
       if (nums[j] === nums[i]) {
