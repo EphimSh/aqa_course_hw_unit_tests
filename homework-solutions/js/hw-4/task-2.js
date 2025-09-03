dff0a8c405f9d5e014558ac233c51ab97ade2bd5
EphimSh <ephimizm@gmail.com>
2025-09-03 21:56:16 +0300

    fix if age > maxAge condition

--- homework-solutions/js/hw-4/task-2.js
+++ homework-solutions/js/hw-4/task-2.js
@@ -32,12 +32,12 @@ if (age < minAge) {
 const testData = [10, 17, 18, 19, 59, 60, 61];
 testData.forEach((age) => {
   if (age < minAge) {
-  console.log(`You don't have access cause your age is ${age} It's less then ${minAge}`);
-} else if (age >= minAge && age < maxAge) {
-  console.log(`Welcome  !`);
-} else if (age >= maxAge) {
-  console.log(`Keep calm and look Culture channel`);
-} else {
-  console.log(`Technical work`);
-}
+    console.log(`You don't have access cause your age is ${age} It's less then ${minAge}`);
+  } else if (age >= minAge && age < maxAge) {
+    console.log(`Welcome  !`);
+  } else if (age > maxAge) {
+    console.log(`Keep calm and look Culture channel`);
+  } else {
+    console.log(`Technical work`);
+  }
 });
