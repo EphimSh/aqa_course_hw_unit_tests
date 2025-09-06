26c9f92fe2d51b567737d04dc8920854e450d86b
EphimSh <ephimizm@gmail.com>
2025-09-06 14:01:29 +0300

    add hw11 task1 solution

--- homework-solutions/js/hw-11/task-1.mjs
+++ homework-solutions/js/hw-11/task-1.mjs
@@ -32,11 +32,13 @@ class Employee {
     if (typeof value != 'string') throw new Error(`${value} is not a string`);
     this._profession = value;
   }
-  setSalary(value) {
-    this.#salary += value;
+  set salary(value) {
+    if (typeof value != 'number') throw new Error(`${value} is not a number`);
+    if (value < 0) throw new Error(`${value} should be positive number`);
+    this.#salary = value;
   }
   getFullName() {
-    return `${this.lastName} ${this.firstName}`;
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
@@ -44,17 +46,60 @@ const emp1 = new Employee('Darina', 'Volkova', 'L3 Engineer', 1800);
 const emp2 = new Employee('Vasilisa', 'Volkova', 'L2 Engineer', 1200);
 const emp3 = new Employee('Galina', 'Pupkova', 'L1 Engineer', 800);
 const empArr = [emp1, emp2, emp3];
-empArr.forEach((obj) => console.log(obj.firstName));
 
 empArr.forEach((obj) => {
-  obj.profession = 'QA';
-  obj.setSalary(2000);
+  obj.profession = 'QA Engineer';
+  obj.salary = 3000;
 });
 
-empArr.forEach((obj) => console.log(`${obj.firstName} ${obj.lastName} ${obj.profession} ${obj.salary}`));
+empArr.forEach((obj) =>
+  console.log(`Fullname: ${obj._firstName} ${obj._lastName}\nProfession: ${obj._profession}\nSalary: ${obj.salary}\n`),
+);
 
 class Company {
-  // Ваш код
+  #employees = [];
+  constructor(title, phone, address, employees) {
+    this.title = title;
+    this.phone = phone;
+    this.address = address;
+    this.#employees = Array.isArray(employees) ? employees : [];
+  }
+
+  get title() {
+    return this._title;
+  }
+  get phone() {
+    return this._phone;
+  }
+  get address() {
+    return this._address;
+  }
+
+  set title(value) {
+    if (typeof value != 'string') throw new Error(`${value} is not type of string`);
+    this._title = value;
+  }
+
+  set phone(value) {
+    if (isNaN(Number(value))) throw new Error(`${value} is not a number`);
+    this._phone = value;
+  }
+
+  set address(value) {
+    if (typeof value != 'string') throw new Error(`${value} is not a string`);
+    this._address = value;
+  }
+
+  addEmployee(employee) {
+    if (!(employee instanceof Employee)) throw new Error(`${empployee} is not instanceof Employee.class`);
+    this.#employees.push(employee);
+  }
+  getEmployees() {
+    return this.#employees;
+  }
+  getInfo() {
+    return `Компания: ${this._title}\nАдрес: ${this._address}\nКоличество сотрудников: ${this.#employees.length}`;
+  }
 }
 
-// export { Employee, Company };
+export { Employee, Company };
